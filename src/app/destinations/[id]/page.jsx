f919904f96827c055f7d5aa22a82e8e1ebb2da0f
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-05-20 18:05:59 +0600

    fixed some bugs

--- src/app/destinations/[id]/page.jsx
+++ src/app/destinations/[id]/page.jsx
@@ -1,5 +1,6 @@
-import { Separator } from "@heroui/react";
+import { Button, Separator } from "@heroui/react";
 import Image from "next/image";
+import { BiEdit } from "react-icons/bi";
 import { BsCalendar2Date } from "react-icons/bs";
 import { FiArrowUpRight } from "react-icons/fi";
 import { MdOutlineLocationOn } from "react-icons/md";
@@ -21,13 +22,16 @@ const DestinationDetailsPage = async({params}) => {
       } = data;
       return (
         <div className="space-y-5 max-w-225 mt-4 mx-auto">
+            <div className="flex justify-end">
+                <Button variant="outline" className={'rounded-none'}><BiEdit/> Edit</Button>
+            </div>
           <div>
             <Image
               alt={destinationName}
               src={imageUrl}
               width={900}
               height={400}
-              className="mx-auto"
+              className="mx-auto w-full h-100 object-cover"
             />
           </div>
           <Separator />

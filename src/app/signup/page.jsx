a9045131f7ea53c714a886ee4bd0eb491c39de27
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-05-21 16:03:35 +0600

    fixed some bugs

---
+++ src/app/signup/page.jsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import {
+  Button,
+  Card,
+  Description,
+  FieldError,
+  Form,
+  Input,
+  Label,
+  TextField,
+} from "@heroui/react";
+
+const SignUpPage = () => {
+  const onSubmit = async(e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const user = Object.fromEntries(formData.entries());
+    
+    const {data, error} = await authClient.signUp.email({
+        email: user.email,
+        password: user.password,
+        name: user.name,
+        image: user.image
+    });
+
+    console.log({data,error});
+  };
+
+  return (
+    <div className="max-w-105 mx-auto space-y-4 mt-5">
+        <div className="text-center">
+            <h1 className="text-2xl font-semibold">Create Account</h1>
+            <p className="text-zinc-500">Start your adventure with Wanderlust</p>
+        </div>
+      <Card className=" rounded-none border">
+        
+        <Form className="flex w-96 flex-col gap-4 mx-auto" onSubmit={onSubmit}>
+          <TextField
+            isRequired
+            name="name"
+            type="text"
+          >
+            <Label>Full Name</Label>
+            <Input placeholder="Enter your name" />
+            <FieldError />
+          </TextField>
+          <TextField
+            name="image"
+            type="url"
+          >
+            <Label>Image URL</Label>
+            <Input placeholder="Enter image url" />
+            <FieldError />
+          </TextField>
+          <TextField
+            isRequired
+            name="email"
+            type="email"
+            validate={(value) => {
+              if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+                return "Please enter a valid email address";
+              }
+
+              return null;
+            }}
+          >
+            <Label>Email Address</Label>
+            <Input placeholder="john@example.com" />
+            <FieldError />
+          </TextField>
+
+          <TextField
+            isRequired
+            minLength={8}
+            name="password"
+            type="password"
+            validate={(value) => {
+              if (value.length < 8) {
+                return "Password must be at least 8 characters";
+              }
+              if (!/[A-Z]/.test(value)) {
+                return "Password must contain at least one uppercase letter";
+              }
+              if (!/[0-9]/.test(value)) {
+                return "Password must contain at least one number";
+              }
+
+              return null;
+            }}
+          >
+            <Label>Password</Label>
+            <Input placeholder="Enter your password" />
+            <Description>
+              Must be at least 8 characters with 1 uppercase and 1 number
+            </Description>
+            <FieldError />
+          </TextField>
+
+          <div className="flex gap-2">
+            <Button className={'bg-cyan-500 hover:bg-cyan-400 rounded-none w-full'} type="submit">
+              Create Account
+            </Button>
+          </div>
+        </Form>
+      </Card>
+    </div>
+  );
+}
+
+export default SignUpPage;

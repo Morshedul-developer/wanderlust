927652e7e8a3ba367d79a192d114c9093496ed3c
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-05-22 18:30:34 +0600

    created the my bookings page and used the created api

--- src/app/my-bookings/page.jsx
+++ src/app/my-bookings/page.jsx
@@ -1,10 +1,30 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
-const MyBookingsPage = () => {
-    return (
-        <div>
-            MyBookingsPage
-        </div>
-    );
+const MyBookingsPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  const user = session?.user;
+  const res = await fetch(`http://localhost:5000/bookings/${user?.id}`);
+  const bookings = await res.json();
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="text-3xl font-bold">My Bookings</h1>
+        <p className="text-muted mt-2">
+          Manage and view your upcoming travel plans
+        </p>
+      </div>
+      <div>
+        {
+            bookings.map((booking,index) => <div key={index}>
+                <h3>{booking.userName}</h3>
+            </div>)
+        }
+      </div>
+    </div>
+  );
 };
 
 export default MyBookingsPage;

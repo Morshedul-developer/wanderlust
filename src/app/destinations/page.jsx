af2323ff6609c1fc7fd6bcf0bd52ed8eb8bd2f21
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-06-09 21:43:50 +0600

    fixed a bug

--- src/app/destinations/page.jsx
+++ src/app/destinations/page.jsx
@@ -6,7 +6,7 @@ const DestinationsPage = async() => {
 
     return (
         <div className="max-w-7xl mx-auto space-y-4 mt-10 mb-15">
-            <h1 className="capitalize text-3xl font-bold">Explore all destinations</h1>
+            <h1 className="capitalize text-4xl font-bold">Explore all destinations</h1>
             <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6">
                 {
                     destinations.map(destination => <DestinationCard key={destination._id} destination={destination}/>)
